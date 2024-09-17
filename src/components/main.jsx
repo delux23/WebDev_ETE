cf7cdd88872a062f075943e848c959fcdbf1dc47
unknown <nagpald316@gmail.com>
2024-09-17 17:11:31 +0530

    ete

---
+++ src/components/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="https://images.unsplash.com/photo-1543728069-a3f97c5a2f32?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+              Elevate your style with the latest in luxury fashion
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        At LVMH, we blend fashion with innovation, inspired by the vision of Bernard Arnault, a pioneer in luxury and excellence. Our e-commerce store brings you curated collections that embody sophistication, style, and elegance. As a part of the legacy that Mr. Arnault has built, we are dedicated to offering you the finest in fashion, delivering premium quality clothing with a commitment to exceptional customer experience. Discover luxury reimagined, only at LVMH.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
